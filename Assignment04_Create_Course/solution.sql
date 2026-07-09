eced37d048fbc977aa8e92ac82f37917b0e8e33e
SabariVarshan07 <sabarivarshan47@gmail.com>
2026-07-09 12:29:39 +0530

    Create Course table and insert initial records
    Created a Course table and inserted three records with their details. Added a command to display the structure of the Course table.

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,13 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Credits INT,
+    DepartmentID INT
+);
+          
+insert INTO Course (CourseID, CourseName, Credits, DepartmentID)
+			VALUES(101, 'Database Management System', 4, 1),
+				(102, 'Computer Networks', 3, 1),
+			 (103, 'Operating Systems', 4, 2);
+       describe Course
+           
